a8614cead90a6aeed42c494c863be548cbbe03da
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-07-23 18:33:55 +0300

    Add AnswerController and View

---
+++ PollingSystem/Controllers/AnswerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Polling.DataAccessLayer.Data;
+using Polling.DataAccessLayer.Models;
+
+namespace PollingSystem.Controllers
+{
+    public class AnswerController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AnswerController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: Answer/Index
+        public ActionResult Index()
+        {
+            var answers = _dbContext.Answers.Include(a => a.Questions).ToList();
+            return View(answers);
+        }
+    }
+}
