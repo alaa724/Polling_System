afabb00911a6373bf4eb15629f982701bc016cb2
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-07-24 19:59:38 +0300

    Refactor QuestionController

--- PollingSystem/Controllers/QuestionController.cs
+++ PollingSystem/Controllers/QuestionController.cs
@@ -14,27 +14,32 @@ namespace PollingSystem.Controllers
             _dbContext = dbContext;
         }
 
-        // GET: Question/AddToPoll/5
-        public ActionResult AddToPoll(int pollId)
-        {
-            var poll = _dbContext.Polls.Find(pollId);
-            if (poll == null)
-                return NotFound();
 
-            var question = new Question { PollId = pollId };
+        // GET: Question/Index/5 
+        public ActionResult Index(int pollId)
+        {
+            var questions = _dbContext.Questions.Where(q => q.PollId == pollId).ToList();
+            return View(questions);
+        }
+
+
+        // GET: Question/Create
+        public ActionResult Create(int pollId)
+        {
+            Question question = new Question { PollId = pollId };
             return View(question);
         }
 
-        // POST: Question/AddToPoll/5
+        // POST: Questions/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddToPoll(int pollId, Question question)
+        public ActionResult Create(Question question)
         {
             if (ModelState.IsValid)
             {
                 _dbContext.Questions.Add(question);
                 _dbContext.SaveChanges();
-                return RedirectToAction("Index", "Home"); // Redirect to home page 
+                return RedirectToAction("AddQuestions", "Polls", new { id = question.PollId });
             }
 
             return View(question);
